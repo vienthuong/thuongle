dab38d131a1249b76106ec1c8ff45d99713d27c3
ThuongLe <levienthuong@gmail.com>
2017-06-05 17:34:53 +0700

    chat

---
+++ resources/views/frontend/master.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Chat</title>
+    <script type="text/javascript" src="bower_assets/jquery/dist/jquery.min.js"></script>
+    <script type="text/javascript" src="bower_assets/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/chat.js"></script>
+    <link rel="stylesheet" type="text/css" href="bower_assets/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="bower_assets/perfect-scrollbar/css/perfect-scrollbar.min.css">
+        <link rel="stylesheet" type="text/css" href="css/chat.css">
+    <script type="text/javascript" src="bower_assets/perfect-scrollbar/js/perfect-scrollbar.jquery.min.js"></script>
+    <script type="text/javascript" src="js/scripts.js"></script>
+</head>
+<body>
+  @yield('main-content')
+</body>
+</html>
