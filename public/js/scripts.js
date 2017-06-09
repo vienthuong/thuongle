eddd88e853daa1f0d4b21dd1f9e7cbcfa36b054a
Thuong Le <levienthuong@gmail.com>
2017-06-09 19:18:02 +0700

    Socket herroku

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,4 +1,4 @@
- var socket = io();
+ var socket = io('https://thuongle.herokuapp.com:6002');
   socket.on('news', function (data) {
     console.log(data);
     // socket.emit('my other event', { my: data });
