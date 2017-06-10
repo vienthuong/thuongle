8471df5fbd935fb853228027b29b5887127c6c29
Thuong Le <levienthuong@gmail.com>
2017-06-11 00:15:33 +0700

    ABC

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,4 +1,4 @@
- var socket = io(window.location.hostname + ':3000');
+ var socket = io();
  // var socket = io();
       var el = document.getElementById('server-time');
       socket.on('time', function(timeString) {
