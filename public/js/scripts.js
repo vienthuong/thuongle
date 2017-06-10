6ac1a707fc9ec5eb72645cfbb6ce943192fd18b1
Thuong Le <levienthuong@gmail.com>
2017-06-11 00:36:28 +0700

    adasd

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,5 +1,5 @@
- var socket = io();
- // var socket = io();
+ // var socket = io(window.location.hostname + ':3000');
+ var socket = io.connect(window.location.hostname + ':6001');
       var el = document.getElementById('server-time');
       socket.on('time', function(timeString) {
         console.log('Server time: ' + timeString);
