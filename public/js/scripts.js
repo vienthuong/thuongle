c7bed093070825beaa564522684d26f9e1945d39
Thuong Le <levienthuong@gmail.com>
2017-06-10 20:36:43 +0700

    Socket

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,4 +1,4 @@
- var socket = io('http://localhost:3000');
+ var socket = io(window.location.hostname + ':3000');
       var el = document.getElementById('server-time');
       socket.on('time', function(timeString) {
         console.log('Server time: ' + timeString);
